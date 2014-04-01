02202b6fea3758985a9e9a5072c1ed1082a7daae
SIA_GEODESY <siavash.ghelichkhan@tum.de>
2014-04-01 15:45:00 +0200

    Removal of extra files

--- Filter.m
+++
@@ -1,7 +0,0 @@
-Cut_degree=5;
-max_degree=100;
-gauss_radius = 2000/Cut_degree;
-GAUSS_FILTER = ...
-filterCoefficientsGaussian(gauss_radius,max_degree);
-
-figure;plot([1:length(GAUSS_FILTER)], GAUSS_FILTER);
